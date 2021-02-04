10e5dd660cce51db5874cd45e6b4d8fd1842f441
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-02-03 21:46:52 -0800

    bump version

--- index.ts
+++ index.ts
@@ -16,14 +16,14 @@ const vpc = new awsx.ec2.Vpc("mina-vpc", {});
 const cluster = new eks.Cluster("mina-private-chain", {
     vpcId: vpc.id,
     subnetIds: vpc.publicSubnetIds,
-    instanceType: "c5.xlarge",
-    desiredCapacity: 2,
+    instanceType: "c5.2xlarge",
+    desiredCapacity: 1,
 
 })
 
 const public_key = fs.readFileSync('key.pub', 'utf8');
 const private_key = fs.readFileSync('key', 'utf8');
-const private_key_password = fs.readFileSync('password', 'utf8');
+const private_key_password = fs.readFileSync('password', 'utf8').replace(/\n/g, '');
 
 const minaKey = new kx.Secret("mina-secret", {
     stringData: {
@@ -35,21 +35,25 @@ const minaKey = new kx.Secret("mina-secret", {
 const block_producer_helm_values_file = fs.readFileSync('block_producer_values.yaml', 'utf8')
 const block_producer_helm_values = YAML.parse(block_producer_helm_values_file)
 const blockProducer = new k8s.helm.v2.Chart("block-producer", {
-    chart: "block-producer",
-    fetchOpts: {
-      repo: "https://coda-charts.storage.googleapis.com" },
-    //path: "../mina/helm/block-producer",
+    //chart: "block-producer",
+    //fetchOpts: {
+      //repo: "https://coda-charts.storage.googleapis.com" },
+    path: "../mina/helm/block-producer",
     values: {
         "testnetName": "testworld",
         "coda": {
-            "image": "minaprotocol/mina-daemon-baked:0.2.11-d075f83-testworld-d075f83",
+            "image": "minaprotocol/mina-daemon-baked:0.2.12-718eba4-testworld-6276828",
             "privkeyPass": private_key_password,
             "logLevel": "info",
             "runtimeConfig": '{ "daemon": { "log-block-creation": false, "super-catchup": true }}',
             "seedPeers": [ "/dns4/seed-one.testworld.o1test.net/tcp/10001/p2p/12D3KooWJ9mNdbUXUpUNeMnejRumKzmQF15YeWwAPAhTAWB6dhiv",
                 "/dns4/seed-two.testworld.o1test.net/tcp/10001/p2p/12D3KooWFcGGeUmbmCNq51NBdGvCWjiyefdNZbDXADMK5CDwNRm5",
-                "/ip4/104.198.77.98/tcp/10516/p2p/12D3KooWKatyUTf78AWcxoK3Gt1JdrmyXjHvZVnxH2uBziKFdPEt",
-                "/ip4/35.222.191.7/tcp/10517/p2p/12D3KooWMNkjkUWGuJoopGX2J91wNuhb4FGJ99BdA6ZivM7bVDex"]
+                "/ip4/34.122.3.199/tcp/10516/p2p/12D3KooWHhAfnVh3unXQTLTXqkNFqbrWTNU2LfbvzBHa8uvTkC9P",
+                "/ip4/104.154.42.130/tcp/10517/p2p/12D3KooWCtpkMEtbZohzojAnaz55WU8vMcpDHToUxy34WqvdN6fU",
+                "/ip4/35.224.110.60/tcp/10518/p2p/12D3KooWFL4VPmY4ysANUVXQCYVXDCprWGGa9sXyJiMfPWW8JLka",
+                "/ip4/34.68.171.184/tcp/10519/p2p/12D3KooWPyrhmzj6cGxKEWvfh7QB8xBu8Hqo79ctcX6GjqNxQKoh",
+                "/ip4/34.123.121.178/tcp/10520/p2p/12D3KooWPdotyVS9BBNFCHdnnZR97TgCZB75XahRwKRnDzCUzQhB",
+                "/ip4/35.223.59.114/tcp/10909/p2p/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr"],
         },
         "nodeSelector": {"preemptible": false},
         "blockProducerConfigs": [
